4af970c66f0fc6f25dc77aa0c30322473a5e4951
Tamunokuro <tamunokrobluejack@gmail.com>
2023-03-04 17:42:01 +0100

    Updatee build file

--- payment/settings.py
+++ payment/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2yd)tlf*e)(82zt7-r7sa9v(@_ohn100%-)@x^2hidu3%y*aj*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
